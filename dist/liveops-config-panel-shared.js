00f417ce974f9fd23ae6e84798c492cca70d0715
Phil Hachey <phil.hachey@bluespurs.com>
2015-12-11 10:22:11 -0400

    Fixed merge issue

--- dist/liveops-config-panel-shared.js
+++ dist/liveops-config-panel-shared.js
@@ -56,6 +56,8 @@ angular.module('liveopsConfigPanel.shared.config')
 
 .constant('uuidcacheKey', 'LIVEOPS-CACHE-KEY')
 
+.constant('apiErrorKeys', ['required'])
+
 ;
 
 'use strict';
@@ -1506,6 +1508,56 @@ angular.module('liveopsConfigPanel.shared.directives')
   }]);
 
 'use strict';
+
+angular.module('liveopsConfigPanel.shared.directives')
+  .directive('scrollTable', [function() {
+    return {
+      restrict: 'A',
+      replace: 'true',
+      compile: function CompilingFunction($templateElement) {
+        $templateElement.removeAttr('scroll-table'); //Prevent infinite recursion
+
+        var headerHeight = $templateElement.find('thead').height();
+        if (headerHeight === 0){
+          headerHeight = 35;
+        }
+
+        var headerCopy = $templateElement.find('thead').clone(true, true);
+        headerCopy.find('th').css('height', headerHeight + 'px');
+
+        var cloneHeaderTable = angular.element('<table class="clone-header">' + headerCopy[0].outerHTML + '</table>');
+        var origClasses = $templateElement[0].className;
+        cloneHeaderTable.addClass(origClasses);
+
+        //Remove duplicated header inputs for cleaner HTML
+        //Note: if a cell contains only an input and has no width explicitly set,
+        //removing the input will cause misalignment between the table cells and the header cells.
+        $templateElement.find('thead').find('input').remove();
+
+        $templateElement.replaceWith('<div class="scrollable-table-container" style="padding-top:' + headerHeight + 'px;">' +
+            cloneHeaderTable[0].outerHTML +
+            '<div class="table-wrapper"><div>' + $templateElement[0].outerHTML + '</div></div>' +
+            '</div>');
+
+        return function($scope, element, attrs){
+          if (attrs.maxHeight){
+            $scope.$watch(function(){return element.find('tbody').find('tr').length;}, function(count){
+              if (count > 0){
+                var approxHeight = headerHeight * count;
+                if (approxHeight < attrs.maxHeight){
+                  element.css('height', approxHeight + headerHeight + 5 + 'px');
+                } else {
+                  element.css('height', attrs.maxHeight + 'px');
+                }
+              }
+            });
+          }
+        };
+      }
+    };
+  }]);
+
+'use strict';
 /*jslint browser:true */
 
 angular.module('liveopsConfigPanel.shared.directives')
@@ -1597,56 +1649,6 @@ angular.module('liveopsConfigPanel.shared.directives')
       }
     };
   }]);
-'use strict';
-
-angular.module('liveopsConfigPanel.shared.directives')
-  .directive('scrollTable', [function() {
-    return {
-      restrict: 'A',
-      replace: 'true',
-      compile: function CompilingFunction($templateElement) {
-        $templateElement.removeAttr('scroll-table'); //Prevent infinite recursion
-
-        var headerHeight = $templateElement.find('thead').height();
-        if (headerHeight === 0){
-          headerHeight = 35;
-        }
-
-        var headerCopy = $templateElement.find('thead').clone(true, true);
-        headerCopy.find('th').css('height', headerHeight + 'px');
-
-        var cloneHeaderTable = angular.element('<table class="clone-header">' + headerCopy[0].outerHTML + '</table>');
-        var origClasses = $templateElement[0].className;
-        cloneHeaderTable.addClass(origClasses);
-
-        //Remove duplicated header inputs for cleaner HTML
-        //Note: if a cell contains only an input and has no width explicitly set,
-        //removing the input will cause misalignment between the table cells and the header cells.
-        $templateElement.find('thead').find('input').remove();
-
-        $templateElement.replaceWith('<div class="scrollable-table-container" style="padding-top:' + headerHeight + 'px;">' +
-            cloneHeaderTable[0].outerHTML +
-            '<div class="table-wrapper"><div>' + $templateElement[0].outerHTML + '</div></div>' +
-            '</div>');
-
-        return function($scope, element, attrs){
-          if (attrs.maxHeight){
-            $scope.$watch(function(){return element.find('tbody').find('tr').length;}, function(count){
-              if (count > 0){
-                var approxHeight = headerHeight * count;
-                if (approxHeight < attrs.maxHeight){
-                  element.css('height', approxHeight + headerHeight + 5 + 'px');
-                } else {
-                  element.css('height', attrs.maxHeight + 'px');
-                }
-              }
-            });
-          }
-        };
-      }
-    };
-  }]);
-
 'use strict';
 
 angular.module('liveopsConfigPanel.shared.directives')
@@ -4789,86 +4791,6 @@ angular.module('liveopsConfigPanel.shared.services')
   ]);
 'use strict';
 
-angular.module('liveopsConfigPanel.tenant.group.mock', ['liveopsConfigPanel.mock'])
-  .service('mockGroups', function(Group) {
-    return [new Group({
-      'id': 'groupId1',
-      'name': 'groupName1',
-      'tenantId': 'tenant-id'
-    }), new Group({
-      'id': 'groupId2',
-      'name': 'groupName2',
-      'tenantId': 'tenant-id'
-    }), new Group({
-      'id': 'groupId3',
-      'name': 'groupName3',
-      'tenantId': 'tenant-id'
-    }), new Group({
-      'id': 'groupId100',
-      'name': 'groupName100',
-      'description': 'Does not exist yet!',
-      'tenantId': 'tenant-id'
-    })];
-  })
-  .run(['$httpBackend', 'apiHostname', 'mockGroups',
-    function($httpBackend, apiHostname, mockGroups) {
-      $httpBackend.when('GET', apiHostname + '/v1/tenants/tenant-id/groups/groupId1').respond({
-        'result': mockGroups[0]
-      });
-
-      $httpBackend.when('GET', apiHostname + '/v1/tenants/tenant-id/groups/groupId2').respond({
-        'result': mockGroups[1]
-      });
-      
-      $httpBackend.when('GET', apiHostname + '/v1/tenants/tenant-id/groups/groupId3').respond({
-        'result': mockGroups[2]
-      });
-
-      $httpBackend.when('GET', apiHostname + '/v1/tenants/tenant-id/groups').respond({
-        'result': [mockGroups[0], mockGroups[1], mockGroups[2]]
-      });
-
-      $httpBackend.when('POST', apiHostname + '/v1/tenants/tenant-id/groups').respond({
-        'result': mockGroups[3]
-      });
-
-      $httpBackend.when('GET', apiHostname + '/v1/groups/groupId0').respond(404);
-    }
-  ]);
-
-'use strict';
-
-angular.module('liveopsConfigPanel.shared.services')
-  .factory('Group', ['LiveopsResourceFactory', 'apiHostname', 'cacheAddInterceptor', 'emitInterceptor',
-    function (LiveopsResourceFactory, apiHostname, cacheAddInterceptor, emitInterceptor) {
-
-      var Group = LiveopsResourceFactory.create({
-        endpoint: apiHostname + '/v1/tenants/:tenantId/groups/:id',
-        resourceName: 'Group',
-        updateFields: [{
-          name: 'name'
-        }, {
-          name: 'description',
-          optional: true
-        }, {
-          name: 'owner'
-        }, {
-          name: 'active',
-          optional: true
-        }],
-        saveInterceptor: [cacheAddInterceptor, emitInterceptor],
-        updateInterceptor: emitInterceptor
-      });
-
-      Group.prototype.getDisplay = function () {
-        return this.name;
-      };
-
-      return Group;
-    }
-  ]);
-'use strict';
-
 angular.module('liveopsConfigPanel.shared.services')
   .factory('Integration', ['LiveopsResourceFactory', 'apiHostname', 'emitInterceptor',
     function (LiveopsResourceFactory, apiHostname, emitInterceptor) {
@@ -5009,6 +4931,86 @@ angular.module('liveopsConfigPanel.shared.services')
       };
       
       return List;
+    }
+  ]);
+'use strict';
+
+angular.module('liveopsConfigPanel.tenant.group.mock', ['liveopsConfigPanel.mock'])
+  .service('mockGroups', function(Group) {
+    return [new Group({
+      'id': 'groupId1',
+      'name': 'groupName1',
+      'tenantId': 'tenant-id'
+    }), new Group({
+      'id': 'groupId2',
+      'name': 'groupName2',
+      'tenantId': 'tenant-id'
+    }), new Group({
+      'id': 'groupId3',
+      'name': 'groupName3',
+      'tenantId': 'tenant-id'
+    }), new Group({
+      'id': 'groupId100',
+      'name': 'groupName100',
+      'description': 'Does not exist yet!',
+      'tenantId': 'tenant-id'
+    })];
+  })
+  .run(['$httpBackend', 'apiHostname', 'mockGroups',
+    function($httpBackend, apiHostname, mockGroups) {
+      $httpBackend.when('GET', apiHostname + '/v1/tenants/tenant-id/groups/groupId1').respond({
+        'result': mockGroups[0]
+      });
+
+      $httpBackend.when('GET', apiHostname + '/v1/tenants/tenant-id/groups/groupId2').respond({
+        'result': mockGroups[1]
+      });
+      
+      $httpBackend.when('GET', apiHostname + '/v1/tenants/tenant-id/groups/groupId3').respond({
+        'result': mockGroups[2]
+      });
+
+      $httpBackend.when('GET', apiHostname + '/v1/tenants/tenant-id/groups').respond({
+        'result': [mockGroups[0], mockGroups[1], mockGroups[2]]
+      });
+
+      $httpBackend.when('POST', apiHostname + '/v1/tenants/tenant-id/groups').respond({
+        'result': mockGroups[3]
+      });
+
+      $httpBackend.when('GET', apiHostname + '/v1/groups/groupId0').respond(404);
+    }
+  ]);
+
+'use strict';
+
+angular.module('liveopsConfigPanel.shared.services')
+  .factory('Group', ['LiveopsResourceFactory', 'apiHostname', 'cacheAddInterceptor', 'emitInterceptor',
+    function (LiveopsResourceFactory, apiHostname, cacheAddInterceptor, emitInterceptor) {
+
+      var Group = LiveopsResourceFactory.create({
+        endpoint: apiHostname + '/v1/tenants/:tenantId/groups/:id',
+        resourceName: 'Group',
+        updateFields: [{
+          name: 'name'
+        }, {
+          name: 'description',
+          optional: true
+        }, {
+          name: 'owner'
+        }, {
+          name: 'active',
+          optional: true
+        }],
+        saveInterceptor: [cacheAddInterceptor, emitInterceptor],
+        updateInterceptor: emitInterceptor
+      });
+
+      Group.prototype.getDisplay = function () {
+        return this.name;
+      };
+
+      return Group;
     }
   ]);
 'use strict';
