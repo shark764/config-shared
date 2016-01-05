45869e5f078291ff25984f6679b86bacee8bf838
Phil Hachey <phil.hachey@bluespurs.com>
2016-01-05 17:45:16 -0400

    TITAN2-4716 Added twilio languages and voices

---
+++ src/liveops-config-panel-shared/constants/twilio.js
@@ -0,0 +1,98 @@
+'use strict';
+
+/*global window: false */
+
+angular.module('liveopsConfigPanel.shared.config')
+
+.constant('twilioLangs', [{
+  'value': 'da-DK',
+  'display': 'Danish, Denmark'
+}, {
+  'value': 'de-DE',
+  'display': 'German, Germany'
+}, {
+  'value': 'en-AU',
+  'display': 'English, Australia'
+}, {
+  'value': 'en-CA',
+  'display': 'English, Canada'
+}, {
+  'value': 'en-GB',
+  'display': 'English, UK'
+}, {
+  'value': 'en-IN',
+  'display': 'English, India'
+}, {
+  'value': 'en-US',
+  'display': 'English, United States'
+}, {
+  'value': 'ca-ES',
+  'display': 'Catalan, Spain'
+}, {
+  'value': 'es-ES',
+  'display': 'Spanish, Spain'
+}, {
+  'value': 'es-MX',
+  'display': 'Spanish, Mexico'
+}, {
+  'value': 'fi-FI',
+  'display': 'Finnish, Finland'
+}, {
+  'value': 'fr-CA',
+  'display': 'French, Canada'
+}, {
+  'value': 'fr-FR',
+  'display': 'French, France'
+}, {
+  'value': 'it-IT',
+  'display': 'Italian, Italy'
+}, {
+  'value': 'ja-JP',
+  'display': 'Japanese, Japan'
+}, {
+  'value': 'ko-KR',
+  'display': 'Korean, Korea'
+}, {
+  'value': 'nb-NO',
+  'display': 'Norwegian, Norway'
+}, {
+  'value': 'nl-NL',
+  'display': 'Dutch, Netherlands'
+}, {
+  'value': 'pl-PL',
+  'display': 'Polish-Poland'
+}, {
+  'value': 'pt-BR',
+  'display': 'Portuguese, Brazil'
+}, {
+  'value': 'pt-PT',
+  'display': 'Portuguese, Portugal'
+}, {
+  'value': 'ru-RU',
+  'display': 'Russian, Russia'
+}, {
+  'value': 'sv-SE',
+  'display': 'Swedish, Sweden'
+}, {
+  'value': 'zh-CN',
+  'display': 'Chinese (Mandarin)'
+}, {
+  'value': 'zh-HK',
+  'display': 'Chinese (Cantonese)'
+}, {
+  'value': 'zh-TW',
+  'display': 'Chinese (Taiwanese Mandarin)'
+}])
+
+.constant('twilioVoices', [{
+  'value': 'man',
+  'display': 'man'
+}, {
+  'value': 'woman',
+  'display': 'woman'
+}, {
+  'value': 'alice',
+  'display': 'alice'
+}])
+
+;
