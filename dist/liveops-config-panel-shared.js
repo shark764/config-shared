f6631801ff457f0297e1d6eea24a8d53b92e9ee2
samantha.routledge <samantha.routledge@bluespurs.com>
2016-01-15 16:07:55 -0400

    Tweak style

--- dist/liveops-config-panel-shared.js
+++ dist/liveops-config-panel-shared.js
@@ -5394,83 +5394,6 @@ angular.module('liveopsConfigPanel.tenant.integration.mock', ['liveopsConfigPane
 
 'use strict';
 
-angular.module('liveopsConfigPanel.tenant.listType.mock', ['liveopsConfigPanel.mock'])
-  .service('mockListTypes', ['ListType', function (ListType) {
-    return [new ListType({
-      'id': 'listTypeId1',
-      'fields': [{
-        'type': 'string',
-        'name': 'field1',
-        'label': 'String field',
-        'required': true
-      }, {
-        'type': 'number',
-        'name': 'field2',
-        'label': 'Number field',
-        'required': false
-      }, {
-        'type': 'boolean',
-        'name': 'field3',
-        'label': 'Bool field',
-        'required': false
-      }]
-    }), new ListType({
-      'id': 'listTypeId2',
-      'fields': [{
-        'type': 'boolean',
-        'name': 'field1',
-        'label': 'Bool field',
-        'required': true
-      }]
-    }), new ListType({
-      'id': 'listTypeId3',
-    })];
-  }])
-  .run(['$httpBackend', 'apiHostname', 'mockListTypes',
-    function ($httpBackend, apiHostname, mockListTypes) {
-      $httpBackend.when('GET', apiHostname + '/v1/tenants/tenant-id/list-types/listTypeId1').respond({
-        'result': mockListTypes[0]
-      });
-
-      $httpBackend.when('GET', apiHostname + '/v1/tenants/tenant-id/list-types/listTypeId2').respond({
-        'result': mockListTypes[1]
-      });
-
-      $httpBackend.when('GET', apiHostname + '/v1/tenants/tenant-id/list-types').respond({
-        'result': [mockListTypes[0], mockListTypes[1]]
-      });
-    }
-  ]);
-'use strict';
-
-angular.module('liveopsConfigPanel.shared.services')
-  .factory('ListType', ['LiveopsResourceFactory', 'apiHostname', 'emitInterceptor', 'emitErrorInterceptor', 'queryCache', 'cacheAddInterceptor',
-    function (LiveopsResourceFactory, apiHostname, emitInterceptor, emitErrorInterceptor, queryCache, cacheAddInterceptor) {
-
-      var List = LiveopsResourceFactory.create({
-        endpoint: apiHostname + '/v1/tenants/:tenantId/list-types/:id',
-        resourceName: 'ListType',
-        updateFields: [{
-          name: 'name'
-        }, {
-          name: 'description',
-          optional: true
-        }],
-        getInterceptor: emitErrorInterceptor,
-        queryInterceptor: emitErrorInterceptor,
-        saveInterceptor: [emitInterceptor, cacheAddInterceptor],
-        updateInterceptor: emitInterceptor
-      });
-
-      List.prototype.getDisplay = function () {
-        return this.name;
-      };
-      
-      return List;
-    }
-  ]);
-'use strict';
-
 angular.module('liveopsConfigPanel.shared.services')
   .service('itemBackupInterceptor', [function () {
     this.response = function (response) {
@@ -5550,6 +5473,83 @@ angular.module('liveopsConfigPanel.shared.services')
         queryInterceptor: [itemBackupInterceptor, emitErrorInterceptor],
         saveInterceptor: [emitInterceptor, cacheAddInterceptor, itemBackupInterceptor],
         updateInterceptor: [emitInterceptor, itemBackupInterceptor]
+      });
+
+      List.prototype.getDisplay = function () {
+        return this.name;
+      };
+      
+      return List;
+    }
+  ]);
+'use strict';
+
+angular.module('liveopsConfigPanel.tenant.listType.mock', ['liveopsConfigPanel.mock'])
+  .service('mockListTypes', ['ListType', function (ListType) {
+    return [new ListType({
+      'id': 'listTypeId1',
+      'fields': [{
+        'type': 'string',
+        'name': 'field1',
+        'label': 'String field',
+        'required': true
+      }, {
+        'type': 'number',
+        'name': 'field2',
+        'label': 'Number field',
+        'required': false
+      }, {
+        'type': 'boolean',
+        'name': 'field3',
+        'label': 'Bool field',
+        'required': false
+      }]
+    }), new ListType({
+      'id': 'listTypeId2',
+      'fields': [{
+        'type': 'boolean',
+        'name': 'field1',
+        'label': 'Bool field',
+        'required': true
+      }]
+    }), new ListType({
+      'id': 'listTypeId3',
+    })];
+  }])
+  .run(['$httpBackend', 'apiHostname', 'mockListTypes',
+    function ($httpBackend, apiHostname, mockListTypes) {
+      $httpBackend.when('GET', apiHostname + '/v1/tenants/tenant-id/list-types/listTypeId1').respond({
+        'result': mockListTypes[0]
+      });
+
+      $httpBackend.when('GET', apiHostname + '/v1/tenants/tenant-id/list-types/listTypeId2').respond({
+        'result': mockListTypes[1]
+      });
+
+      $httpBackend.when('GET', apiHostname + '/v1/tenants/tenant-id/list-types').respond({
+        'result': [mockListTypes[0], mockListTypes[1]]
+      });
+    }
+  ]);
+'use strict';
+
+angular.module('liveopsConfigPanel.shared.services')
+  .factory('ListType', ['LiveopsResourceFactory', 'apiHostname', 'emitInterceptor', 'emitErrorInterceptor', 'queryCache', 'cacheAddInterceptor',
+    function (LiveopsResourceFactory, apiHostname, emitInterceptor, emitErrorInterceptor, queryCache, cacheAddInterceptor) {
+
+      var List = LiveopsResourceFactory.create({
+        endpoint: apiHostname + '/v1/tenants/:tenantId/list-types/:id',
+        resourceName: 'ListType',
+        updateFields: [{
+          name: 'name'
+        }, {
+          name: 'description',
+          optional: true
+        }],
+        getInterceptor: emitErrorInterceptor,
+        queryInterceptor: emitErrorInterceptor,
+        saveInterceptor: [emitInterceptor, cacheAddInterceptor],
+        updateInterceptor: emitInterceptor
       });
 
       List.prototype.getDisplay = function () {
