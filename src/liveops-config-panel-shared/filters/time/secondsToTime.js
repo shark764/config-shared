cd3dbae6dcbcae028bb9af7c5f4081b13022b35a
josh-stevens <jcstevens12@outlook.com>
2016-04-11 09:52:36 -0500

    TITAN2-5018 - Add filter for displaying duration

---
+++ src/liveops-config-panel-shared/filters/time/secondsToTime.js
@@ -0,0 +1,26 @@
+'use strict';
+
+angular.module('liveopsConfigPanel.shared.filters')
+  .filter('secondsToTime', function () {
+      return function (input) {
+        var hours, minutes, seconds;
+        var formattedData = '';
+
+        if (input >= 3600) {
+          hours = ('0' + Math.floor((input / 60) / 60)).slice(-2);
+          minutes = ('0' + Math.floor((input / 60) % 60)).slice(-2);
+          seconds = ('0' + Math.floor((input % 60) % 60)).slice(-2);
+          formattedData = hours + ':' + minutes + ':' + seconds;
+        } else if (input >= 60) {
+          minutes = ('0' + Math.floor(input / 60)).slice(-2);
+          seconds = ('0' + Math.floor(input % 60)).slice(-2);
+          formattedData = '00:' + minutes + ':' + seconds;
+        } else {
+          seconds = Math.floor(input);
+          formattedData = '00:00:' + seconds;
+        }
+
+        return formattedData.trim();
+      };
+    }
+);
