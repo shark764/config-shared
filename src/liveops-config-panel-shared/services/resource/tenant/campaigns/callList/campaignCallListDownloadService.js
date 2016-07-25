8d31b919f85cf2e1cc94615ee9030d6a2ca0eb42
kirdua1 <kcraft@liveops.com>
2016-07-25 15:22:15 -0500

    Added download function to the service

--- src/liveops-config-panel-shared/services/resource/tenant/campaigns/callList/campaignCallListDownloadService.js
+++ src/liveops-config-panel-shared/services/resource/tenant/campaigns/callList/campaignCallListDownloadService.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('liveopsConfigPanel.shared.services')
-  .factory('CampaignCallListDownload', ['LiveopsResourceFactory', 'apiHostname', 'cacheAddInterceptor', 'emitInterceptor', 'emitErrorInterceptor',
-    function (LiveopsResourceFactory, apiHostname, cacheAddInterceptor, emitInterceptor, emitErrorInterceptor) {
+  .factory('CampaignCallListDownload', ['LiveopsResourceFactory', 'apiHostname', 'cacheAddInterceptor', 'emitInterceptor', 'emitErrorInterceptor', 'Session',
+    function (LiveopsResourceFactory, apiHostname, cacheAddInterceptor, emitInterceptor, emitErrorInterceptor, Session) {
       var CampaignCallListDownload = LiveopsResourceFactory.create({
         endpoint: apiHostname + '/v1/tenants/:tenantId/campaigns/:campaignId/call-list/download',
         requestUrlFields: {
@@ -15,7 +15,10 @@ angular.module('liveopsConfigPanel.shared.services')
         saveInterceptor: [cacheAddInterceptor, emitInterceptor],
         updateInterceptor: emitInterceptor
       });
-
+      CampaignCallListDownload.prototype.download = function(campaignId){
+        //console.log("FIRED FUNCTION");
+        document.location.href = 'https://' + window.atob(Session.token) + apiHostname + '/v1/tenants/' + Session.tenant.tenantId + '/campaigns/' + campaignId + '/call-list/download.csv';
+      }
       return CampaignCallListDownload;
     }
   ]);
