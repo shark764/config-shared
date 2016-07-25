aea681c21c730db9eda75dab577a4d4080fd407f
Doron Orenstein <dorenstein@liveops.com>
2016-07-25 15:38:29 -0500

    got dnc list working on settings page, at least the front end piece

--- src/liveops-config-panel-shared/services/resource/tenant/campaigns/version/campaignVersionService.js
+++ src/liveops-config-panel-shared/services/resource/tenant/campaigns/version/campaignVersionService.js
@@ -11,7 +11,7 @@ angular.module('liveopsConfigPanel.shared.services')
         }, {
           name: 'defaultTimeZone'
         }, {
-          name: 'doNotCallLists'
+          name: 'doNotContactLists'
         }, {
           name: 'callerID',
           optional: true
