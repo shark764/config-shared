3b47c8ebd2eed360d401c0399fd38a3a0266d3fa
Josh Stevens <jcstevens12@outlook.com>
2016-09-12 13:07:04 -0500

    Update integration service

--- src/liveops-config-panel-shared/services/resource/tenant/integration/integrationService.js
+++ src/liveops-config-panel-shared/services/resource/tenant/integration/integrationService.js
@@ -1,13 +1,15 @@
 'use strict';
 
 angular.module('liveopsConfigPanel.shared.services')
-  .factory('Integration', ['LiveopsResourceFactory', 'apiHostname', 'emitInterceptor', 'emitErrorInterceptor',
-    function (LiveopsResourceFactory, apiHostname, emitInterceptor, emitErrorInterceptor) {
+  .factory('Integration', ['LiveopsResourceFactory', 'apiHostname', 'emitInterceptor', 'emitErrorInterceptor', 'cacheAddInterceptor',
+    function (LiveopsResourceFactory, apiHostname, emitInterceptor, emitErrorInterceptor, cacheAddInterceptor) {
 
       var Integration = LiveopsResourceFactory.create({
         endpoint: apiHostname + '/v1/tenants/:tenantId/integrations/:id',
         resourceName: 'Integration',
         updateFields: [{
+          name: 'description'
+        }, {
           name: 'properties'
         }, {
           name: 'accountSid'
@@ -20,7 +22,7 @@ angular.module('liveopsConfigPanel.shared.services')
         }],
         getInterceptor: emitErrorInterceptor,
         queryInterceptor: emitErrorInterceptor,
-        saveInterceptor: emitInterceptor,
+        saveInterceptor: [emitInterceptor, cacheAddInterceptor],
         updateInterceptor: emitInterceptor
       });
 
