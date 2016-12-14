7ab42011f477cece53a704e324695a73d1b2ac26
Doron Orenstein <jazziseverywhere@gmail.com>
2016-12-14 15:28:20 -0600

    CXV1-6787 - Added new regions for Twilio's Virginia and Oregon interconnect

--- src/liveops-config-panel-shared/services/resource/regions/globalRegionsList.js
+++ src/liveops-config-panel-shared/services/resource/regions/globalRegionsList.js
@@ -5,37 +5,45 @@ angular.module('liveopsConfigPanel.shared.services')
     // make sure to keep the first two objects ('default' and 'gll') as the first two, since
     // our markup depends on their index in this list to construct the drop down menus.
     var regions = [{
-      tenantId: 'default',
+      awsdId: 'default',
       twilioId: 'default',
       display: $translate.instant('twilioRegion.tenantDefault')
     }, {
-      tenantId: null,
+      awsdId: null,
       twilioId: 'gll',
       display: $translate.instant('twilioRegion.lowestLatency')
     }, {
-      tenantId: 'ap-southeast-2',
+      awsdId: 'ap-southeast-2',
       twilioId: 'au1',
       display: $translate.instant('twilioRegion.australia')
     }, {
-      tenantId: 'sa-east-1',
+      awsdId: 'sa-east-1',
       twilioId: 'br1',
       display: $translate.instant('twilioRegion.brazil')
     }, {
-      tenantId: 'us-west-1',
+      awsdId: 'us-west-1',
       twilioId: 'ie1',
       display: $translate.instant('twilioRegion.ireland')
     }, {
-      tenantId: 'ap-northeast-1',
+      awsdId: 'ap-northeast-1',
       twilioId: 'jp1',
       display: $translate.instant('twilioRegion.japan')
     }, {
-      tenantId: 'ap-southeast-1',
+      awsdId: 'ap-southeast-1',
       twilioId: 'sg1',
       display: $translate.instant('twilioRegion.singapore')
     }, {
-      tenantId: 'us-east-1',
+      awsdId: 'us-east-1',
       twilioId: 'us1',
       display: $translate.instant('twilioRegion.usEastCoast')
+    }, {
+      awsdId: null,
+      twilioId: 'us1-tnx',
+      display: $translate.instant('twilioRegion.virginiaInterconnect')
+    }, {
+      awsdId: null,
+      twilioId: 'us2-tnx',
+      display: $translate.instant('twilioRegion.oregonInterconnect')
     }];
 
     return regions;
