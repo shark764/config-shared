4ddc6c7261bddef1b63634ae21fe0bfe3adc8992
Doron Orenstein <jazziseverywhere@gmail.com>
2016-12-15 09:59:20 -0600

    CXV1-6781 - Added new functionality for processing data for custom integrations on Integration page

--- src/liveops-config-panel-shared/services/resource/tenant/integration/integrationService.js
+++ src/liveops-config-panel-shared/services/resource/tenant/integration/integrationService.js
@@ -32,6 +32,54 @@ angular.module('liveopsConfigPanel.shared.services')
         return this.type;
       };
 
+      Integration.prototype.handleAuthMethodSelect = function (scope, authMethod, initialLoad) {
+        // the conditional statement below allows  for situations where
+        // we have to pass the integration data as opposed to the scope of the controller
+        if (scope.hasOwnProperty('id')) {
+          scope.selectedIntegration = scope;
+        }
+
+        if (initialLoad === 'true') {
+          scope.selectedIntegration.authType = authMethod;
+          return;
+        }
+
+        if (authMethod === 'basic') {
+          scope.selectedIntegration.authType = 'token';
+        } else {
+          scope.selectedIntegration.authType = 'basic';
+        }
+      };
+
+      Integration.prototype.deleteExtraneousData = function (scope) {
+        // the conditional statement below allows for situations where
+        // we have to pass the integration data as opposed to the scope of the controller
+        var dataWithoutScope = false;
+        if (scope.hasOwnProperty('id')) {
+          scope.selectedIntegration = scope;
+          dataWithoutScope = true;
+        }
+
+        if (scope.selectedIntegration.description === null) {
+          delete scope.selectedIntegration.description;
+        }
+
+        if (scope.selectedIntegration.authType) {
+          if (scope.selectedIntegration.authType === 'token') {
+            scope.selectedIntegration.properties.username =
+            scope.selectedIntegration.properties.password = '';
+          } else {
+            scope.selectedIntegration.properties.token = '';
+          }
+
+          delete scope.selectedIntegration.authType;
+        }
+
+        if (dataWithoutScope === false && scope.selectedIntegration.type && scope.selectedIntegration.type.hasOwnProperty('value')) {
+          scope.selectedIntegration.type = scope.selectedIntegration.type.value;
+        }
+      };
+
       return Integration;
     }
   ]);
