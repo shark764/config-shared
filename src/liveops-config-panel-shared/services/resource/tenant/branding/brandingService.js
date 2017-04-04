db214436feb127e6f6930d78bb13beffa05f03cd
Alexg2195 <alexg2195@gmail.com>
2017-04-04 09:59:35 -0500

    Added css overrides for main config page

--- src/liveops-config-panel-shared/services/resource/tenant/branding/brandingService.js
+++ src/liveops-config-panel-shared/services/resource/tenant/branding/brandingService.js
@@ -89,23 +89,38 @@ angular.module('liveopsConfigPanel.shared.services')
         var stylesheetIndex = $rootScope.sheet.rules.length;
         var navColor = params.styles.navbar;
         var navTextColor = params.styles.navbarText;
+        var primaryColor = params.styles.primaryColor;
+        var accentColor = params.styles.accentColor;
+        var accentHoverColor = params.styles.accentHoverColor;
 
         if (navColor) {
           addCSSRule('#topnav', 'background-color: ' + navColor + '!important', stylesheetIndex);
           addCSSRule('#topnav .fa-caret-down', 'color: ' + navColor + '!important', stylesheetIndex);
+          addCSSRule('.lo-alert', 'border: 2px solid' + navColor + '!important', stylesheetIndex);
         }
         if (navTextColor) {
           var darkerTextColor = colorLuminance(navTextColor, -0.2);
-          addCSSRule('#topnav .drop-label', 'color: ' + darkerTextColor + '!important', stylesheetIndex);
-          addCSSRule('#topnav #tenant-dropdown .drop-label div', 'border: 1px solid ' + darkerTextColor + '!important', stylesheetIndex);
-          addCSSRule('#topnav .divider', 'border: 1px solid ' + darkerTextColor + '!important', stylesheetIndex);
-          addCSSRule('#welcome i', 'color: ' + darkerTextColor + '!important', stylesheetIndex);
-          addCSSRule('#helpMenu i', 'color: ' + darkerTextColor + '!important', stylesheetIndex);
+          addCSSRule('#topnav .drop-label, #welcome i, #helpMenu i', 'color: ' + darkerTextColor + '!important', stylesheetIndex);
+          addCSSRule('#topnav #tenant-dropdown .drop-label div, #topnav .divider', 'border: 1px solid ' + darkerTextColor + '!important', stylesheetIndex);
 
           addCSSRule('#topnav > ul > li.active .drop-label span, #topnav > ul > li.active #logo, #topnav > ul > li:hover .drop-label span, #topnav > ul > li:hover #logo', 'color: ' + navTextColor + '!important', stylesheetIndex);
           addCSSRule('#topnav > ul > li:not(#welcome):not(#helpMenu):hover .label-icon', 'color: ' + navTextColor + '!important', stylesheetIndex);
-          addCSSRule('#welcome:hover i', 'color: ' + navTextColor + '!important', stylesheetIndex);
-          addCSSRule('#helpMenu i:hover', 'color: ' + navTextColor + '!important', stylesheetIndex);
+          addCSSRule('#welcome:hover i, #helpMenu i:hover', 'color: ' + navTextColor + '!important', stylesheetIndex);
+
+        }
+        if (primaryColor) {
+          addCSSRule('#create-btn, .fa-search, #submit-details-btn', 'background-color: ' + primaryColor + '!important', stylesheetIndex);
+          addCSSRule('.lo-main-text, .btn:not(.btn-primary)', 'color: ' + primaryColor + '!important', stylesheetIndex);
+        }
+        if (accentColor) {
+          addCSSRule('.lo-accent-text i', 'color: ' + accentColor + '!important', stylesheetIndex);
+          addCSSRule('#topnav > ul > li.active .drop-label span, #topnav > ul > li.active #logo, #topnav > ul > li:hover #logo', 'box-shadow: inset 0px -5px 0px 0px ' + accentColor + '!important', stylesheetIndex);
+          addCSSRule('.lo-accent-hover-box-border:hover', 'box-shadow: inset 4px 0px 0px 0px ' + accentColor + '!important', stylesheetIndex);
+          addCSSRule('.lo-accent-hover-border:hover', 'border-color: ' + accentColor + '!important', stylesheetIndex);
+        }
+        if (accentHoverColor) {
+          addCSSRule('#items-table tr:hover, #items-table .lo-highlight, .dropdown-container a:hover, .lo-hover-highlight:hover, .lo-alert', 'background-color: ' + accentHoverColor + '!important', stylesheetIndex);
+          addCSSRule('.lo-alert', 'color: ' + '#656565' + '!important', stylesheetIndex);
         }
 
       };
